1984e6f4a6387b5cd4d489efb28fb364a7bb782a
istiak <istiakahmed024@gmail.com>
2025-10-21 19:56:19 +0600

    issue fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,10 +18,7 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
-    ],
-    "rules": {
-      "@typescript-eslint/no-explicit-any": "off"
-    }
+    ]
   },
 ];
 
